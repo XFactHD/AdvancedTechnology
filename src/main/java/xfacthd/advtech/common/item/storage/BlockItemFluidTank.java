be1293080b8a8dc8108dd2803709812f70939e0b
XFactHD <xfacthd@gmx.de>
2020-09-29 03:36:54 +0200

    Add information about contents to FluidTank item

--- src/main/java/xfacthd/advtech/common/item/storage/BlockItemFluidTank.java
+++ src/main/java/xfacthd/advtech/common/item/storage/BlockItemFluidTank.java
@@ -1,17 +1,44 @@
 package xfacthd.advtech.common.item.storage;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.World;
 import net.minecraftforge.fluids.FluidStack;
+import xfacthd.advtech.client.gui.ScreenMachine;
 import xfacthd.advtech.client.render.ister.RenderItemFluidTank;
 import xfacthd.advtech.common.block.storage.BlockFluidTank;
+
+import java.util.List;
 
 public class BlockItemFluidTank extends BlockItem
 {
     public BlockItemFluidTank(BlockFluidTank block, Properties props)
     {
         super(block, props.setISTER(() -> RenderItemFluidTank::new));
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, World world, List<ITextComponent> tooltip, ITooltipFlag flag)
+    {
+        super.addInformation(stack, world, tooltip, flag);
+
+        CompoundNBT nbt = stack.getChildTag("BlockEntityTag");
+        if (nbt != null)
+        {
+            CompoundNBT content = nbt.getCompound("content");
+            FluidStack fluid = FluidStack.loadFluidStackFromNBT(content);
+
+            ITextComponent fluidName = fluid.isEmpty() ? ScreenMachine.TANK_EMPTY : fluid.getDisplayName();
+            int amount = fluid.getAmount();
+            int capacity = content.getInt("capacity");
+
+            tooltip.add(fluidName);
+            tooltip.add(new StringTextComponent(amount + " mB / " + capacity + " mB"));
+        }
     }
 
     public static FluidStack getContents(ItemStack stack)
