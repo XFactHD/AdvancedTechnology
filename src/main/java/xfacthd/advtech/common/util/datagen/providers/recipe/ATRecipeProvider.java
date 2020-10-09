f72739ed90262356449b8e0eabfe4a860eb0288b
XFactHD <xfacthd@gmx.de>
2020-10-10 01:29:03 +0200

    Add helper for conditional recipes with additional helper for mod compat recipes

--- src/main/java/xfacthd/advtech/common/util/datagen/providers/recipe/ATRecipeProvider.java
+++ src/main/java/xfacthd/advtech/common/util/datagen/providers/recipe/ATRecipeProvider.java
@@ -1,15 +1,20 @@
 package xfacthd.advtech.common.util.datagen.providers.recipe;
 
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.RecipeProvider;
+import net.minecraft.data.*;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.crafting.ConditionalRecipe;
+import net.minecraftforge.common.crafting.conditions.ICondition;
+import net.minecraftforge.common.crafting.conditions.ModLoadedCondition;
 import xfacthd.advtech.AdvancedTechnology;
 import xfacthd.advtech.common.ATContent;
 import xfacthd.advtech.common.data.subtypes.Materials;
 import xfacthd.advtech.common.util.data.TagHolder;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public abstract class ATRecipeProvider extends RecipeProvider
 {
@@ -21,6 +26,8 @@ public abstract class ATRecipeProvider extends RecipeProvider
     protected String getProviderName() { return getRecipePrefix() + "_recipes"; }
 
     protected abstract String getRecipePrefix();
+
+
 
     protected static Ingredient oreAsTag(Materials material)
     {
@@ -40,6 +47,38 @@ public abstract class ATRecipeProvider extends RecipeProvider
         if (material == Materials.GOLD) { return Items.GOLD_INGOT; }
         return ATContent.itemIngot.get(material);
     }
+
+
+
+    /**
+     * Register a recipe wrapped in a {@link ConditionalRecipe}. Useful for mod compat recipes, as those recipes
+     * would otherwise crash if the other mod is not installed
+     *
+     * @param condition The condition for this recipe
+     * @param recipeBuilder The recipe (pass reference to {@code XyzRecipeBuilder.build(Consumer, ResourceLocation)})
+     * @param consumer The recipe consumer given by {@link RecipeProvider#registerRecipes(Consumer)}
+     * @param id The recipe id
+     */
+    protected static void conditionalRecipe(ICondition condition,
+                                            BiConsumer<Consumer<IFinishedRecipe>, ResourceLocation> recipeBuilder,
+                                            Consumer<IFinishedRecipe> consumer,
+                                            ResourceLocation id)
+    {
+        ConditionalRecipe.builder()
+                .addCondition(condition)
+                .addRecipe(callable -> recipeBuilder.accept(callable, id))
+                .build(consumer, id);
+    }
+
+    protected static void modCompatRecipe(String modid,
+                                          BiConsumer<Consumer<IFinishedRecipe>, ResourceLocation> recipeBuilder,
+                                          Consumer<IFinishedRecipe> consumer,
+                                          ResourceLocation id)
+    {
+        conditionalRecipe(new ModLoadedCondition(modid), recipeBuilder, consumer, id);
+    }
+
+
 
     protected ResourceLocation name(String name)
     {
