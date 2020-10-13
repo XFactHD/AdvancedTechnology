fecb9a7e6c84cd159a4d4e4337a63b7db865e1ff
XFactHD <xfacthd@gmx.de>
2020-10-14 00:10:56 +0200

    Fix coordinates for fluid ingredient in LiquifierGuiHandler

--- src/main/java/xfacthd/advtech/common/util/compat/jei/gui_handlers/LiquifierGuiHandler.java
+++ src/main/java/xfacthd/advtech/common/util/compat/jei/gui_handlers/LiquifierGuiHandler.java
@@ -6,9 +6,11 @@ import xfacthd.advtech.client.gui.machine.ScreenLiquifier;
 public class LiquifierGuiHandler extends AdvancedGuiHandler<ScreenLiquifier>
 {
     @Override
-    public Object getIngredientUnderMouse(ScreenLiquifier screen, double mouseX, double mouseY) //FIXME: not called
+    public Object getIngredientUnderMouse(ScreenLiquifier screen, double mouseX, double mouseY)
     {
-        if (mouseX >= 98 && mouseX < 114 && mouseY >= 1 && mouseY < 61)
+        int left = screen.getGuiLeft();
+        int top = screen.getGuiTop();
+        if (mouseX >= left + 98 && mouseX < left + 114 && mouseY >= top + 1 && mouseY < top + 61)
         {
             FluidStack fluid = screen.getContainer().getFluidStored();
             return fluid.isEmpty() ? null : fluid;
