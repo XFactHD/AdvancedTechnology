67d5c975be59840ba810895c06b5ce7691e1d90e
XFactHD <xfacthd@gmx.de>
2021-01-18 18:19:46 +0100

    Fix generators not outputting energy to neighboring blocks

--- src/main/java/xfacthd/advtech/common/tileentity/TileEntityGenerator.java
+++ src/main/java/xfacthd/advtech/common/tileentity/TileEntityGenerator.java
@@ -1,17 +1,51 @@
 package xfacthd.advtech.common.tileentity;
 
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.MathHelper;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.energy.IEnergyStorage;
 import xfacthd.advtech.common.capability.energy.EnergySource;
 import xfacthd.advtech.common.capability.item.MachineItemStackHandler;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 public abstract class TileEntityGenerator extends TileEntityInventoryMachine
 {
+    private final Map<Direction, LazyOptional<IEnergyStorage>> neighborCache = new EnumMap<>(Direction.class);
     protected int productionMult = 1;
+    protected int maxExtract = 0;
     protected int progress = -1;
 
     public TileEntityGenerator(TileEntityType<?> type) { super(type); }
+
+    @Override
+    public void tick()
+    {
+        //noinspection ConstantConditions
+        if (!world.isRemote() && energyHandler.getEnergyStored() > 0)
+        {
+            for (Direction side : Direction.values())
+            {
+                if (supportsEnergyOnSide(side))
+                {
+                    getNeighboringEnergyHandler(side).ifPresent(handler ->
+                    {
+                        int rec = handler.receiveEnergy(energyHandler.extractEnergy(maxExtract, true), false);
+                        if (rec > 0) { energyHandler.extractEnergy(rec, false); }
+                    });
+
+                    if (energyHandler.getEnergyStored() <= 0) { break; }
+                }
+            }
+        }
+
+        super.tick();
+    }
 
     @Override
     public void onLevelChanged()
@@ -19,7 +53,7 @@ public abstract class TileEntityGenerator extends TileEntityInventoryMachine
         int mult = (int)Math.pow(2, level.ordinal());
         productionMult = mult;
         int capacity = getBaseEnergyCapacity() * mult;
-        int maxExtract = getBaseExtract() * mult;
+        maxExtract = getBaseExtract() * mult;
         energyHandler.reconfigure(capacity, 0, maxExtract);
     }
 
@@ -32,6 +66,30 @@ public abstract class TileEntityGenerator extends TileEntityInventoryMachine
 
         energyHandler = new EnergySource(getBaseEnergyCapacity(), getBaseExtract());
     }
+
+    protected LazyOptional<IEnergyStorage> getNeighboringEnergyHandler(Direction side)
+    {
+        if (!supportsEnergyOnSide(side)) { return LazyOptional.empty(); }
+
+        LazyOptional<IEnergyStorage> handler = neighborCache.getOrDefault(side, LazyOptional.empty());
+        if (!handler.isPresent())
+        {
+            //noinspection ConstantConditions
+            TileEntity te = world.getTileEntity(pos.offset(side));
+            if (te != null)
+            {
+                handler = te.getCapability(CapabilityEnergy.ENERGY, side.getOpposite());
+                if (handler.isPresent())
+                {
+                    handler.addListener(itemHandler -> onEnergyNeighborInvalidate(side));
+                    neighborCache.put(side, handler);
+                }
+            }
+        }
+        return handler;
+    }
+
+    private void onEnergyNeighborInvalidate(Direction side) { neighborCache.put(side, LazyOptional.empty()); }
 
     protected abstract int getBaseExtract();
 
